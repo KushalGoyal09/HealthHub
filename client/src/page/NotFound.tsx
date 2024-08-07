45e6245c7c41af77a42c0c1af2281ca345bea8f4
Kushal Goyal <116098036+KushalGoyal09@users.noreply.github.com>
2024-08-07 16:04:37 +0530

    add vercel.json

--- client/src/page/NotFound.tsx
+++ client/src/page/NotFound.tsx
@@ -5,6 +5,7 @@ const NotFound = () => {
         <div className="flex flex-col items-center justify-center min-h-screen bg-white text-gray-800">
             <h1 className="text-6xl font-bold mb-4">404</h1>
             <p className="text-2xl mb-8">Page Not Found</p>
+            <p className="mb-8">If you think that is mistake, may be you are not authorized to access this page</p>
             <Link
                 to="/"
                 className="text-lg text-gray-600 hover:text-gray-800 transition"
