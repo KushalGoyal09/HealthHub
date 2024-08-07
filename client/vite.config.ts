56841d1933c91b5b57d62b3791faf2e411206f66
Kushal Goyal <116098036+KushalGoyal09@users.noreply.github.com>
2024-08-07 16:23:38 +0530

    edit config file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: _plugins,
     server: {
         proxy: {
-            "/api": "http://localhost:3000",
+            "/api": "https://backend-healthhub.kushalgoyal.tech",
         },
     },
     resolve: {
